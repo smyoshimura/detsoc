ebb43e19adc8ae4e34502774a12c3c83384de23c
Stefanie Yoshimura <syoshimura01@gmail.com>
2016-04-10 22:16:31 -0700

    header tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ $(function () {
 
     var wipeAnimation = new TimelineMax()
 
-    .to("#innerwrapper", 1, {x: -(viewwidth * .3333) + "px"})
-    .to("#innerwrapper", 1, {x: -(viewwidth * .6666) + "px"})
+    .to("#innerwrapper", 1, {x: -(viewwidth * 3) + "px"})
+    .to("#innerwrapper", 1, {x: -(viewwidth * 3) + "px"})
 
     new ScrollMagic.Scene({
         triggerElement: "#wrapper",
